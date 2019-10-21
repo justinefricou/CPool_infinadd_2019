320db0baa16cfb513202550541b91d5c1c7bd690
Justine Fricou <jfricou@justine.fricou>
2019-10-21 15:48:42 +0200

    Added structure for test_is_sign.c

---
+++ tests/test_is_sign.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2019
+** infin_add : test_is_sign.c
+** File description:
+** Tests for is_sign
+*/
+
+#include <criterion/criterion.h>
+
+int is_sign(char c);
+
+Test(is_sign, normal_case)
+{
+
+}
+
+Test(is_sign, not_a_sign_case)
+{
+
+}
