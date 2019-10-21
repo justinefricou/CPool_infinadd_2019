f27573c40b4148619c7e588e1518f042b4962f9f
Justine Fricou <jfricou@justine.fricou>
2019-10-21 15:08:57 +0200

    Added tests for function is_digit

---
+++ tests/test_is_digit.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2019
+** infin_add : tests_is_digit.c
+** File description:
+** Tests for is_digit
+*/
+
+#include <criterion/criterion.h>
+
+int is_digit(char c);
+
+Test(is_digit, case_digit)
+{
+    for (char c = '0'; c <= '9'; c++) {
+        cr_expect_eq(is_digit(c), 1);
+    }
+}
+
+Test(is_digit, case_not_digit)
+{
+    cr_expect_eq(is_digit('\0'), 0);
+    cr_expect_eq(is_digit('\n'), 0);
+    cr_expect_eq(is_digit('+'), 0);
+    cr_expect_eq(is_digit('h'), 0);
+    cr_expect_eq(is_digit(' '), 0);
+}
