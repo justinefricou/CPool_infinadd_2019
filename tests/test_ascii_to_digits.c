e215471931ac6a17af4774c2812fe2313aabde44
Justine Fricou <jfricou@justine.fricou>
2019-10-22 12:48:47 +0200

    Added tests for ascii_to_digits function

---
+++ tests/test_ascii_to_digits.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2019
+** infin_add : test_ascii_to_digits.c
+** File description:
+** Tests for function ascii_to_digits
+*/
+
+#include <criterion/criterion.h>
+
+void ascii_to_digits(char *str);
+
+Test(ascii_to_digits, normal_case)
+{
+    char expected1[11];
+    char expected2[2];
+    char *actual1 = strdup("0123456789");
+    char *actual2 = strdup("5");
+
+    for (int i = 0; i < 10; i++) {
+        expected1[i] = i;
+    }
+    expected1[10] = '\0';
+    expected2[0] = 5;
+    expected2[1] = '\0';
+    ascii_to_digits(actual1);
+    ascii_to_digits(actual2);
+    for (int i = 0; i < 10; i++) {
+        cr_expect_eq(actual1[i], expected1[i]);
+    }
+    cr_expect_eq(actual2[0], expected2[0]);
+}
