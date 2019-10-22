7819afac51874116f54ac8efe8cfa55d4d927a33
Justine Fricou <jfricou@justine.fricou>
2019-10-22 17:13:42 +0200

    Added functions to add nubers

--- main.c
+++ main.c
@@ -11,17 +11,81 @@
 #include "include/math_tools.h"
 #include "include/string_tools.h"
 
-void add_strings(char *number1, char *number2, char *result)
+void add_2digits(char *res_dozen, char *res_unit, char *digit1, char *digit2)
 {
-    //!
-    // remplir d'espaces si trop de place dans la chaîne
+    res_dozen[0] = res_dozen[0] + (res_unit[0] + digit1[0] + digit2[0]) / 10;
+    res_unit[0] = (res_unit[0] + digit1[0] + digit2[0]) % 10;
 }
 
-void display_result(char *result, int length_result)
+void add_1digit(char *result_dozen, char *result_unit, char *digit)
+{
+    result_dozen[0] = result_dozen[0] + (result_unit[0] + digit[0]) / 10;
+    result_unit[0] = (result_unit[0] + digit[0]) % 10;
+}
+
+void decrement_indexes(int *i1, int *i2, int *i_res)
+{
+    (*i1)--;
+    (*i2)--;
+    (*i_res)--;
+}
+
+char *add_strings(char *nbr1, char *nbr2, int length_nbr1, int length_nbr2)
+{
+    int i1 = length_nbr1 - 1;
+    int i2 = length_nbr2 - 1;
+    int length_result = 0;
+    int i_res = 0;
+    char *res = NULL;
+
+    length_result = max(length_nbr1, length_nbr2) + 2;
+    i_res = length_result - 1;
+    res = malloc(sizeof(char) * (length_result + 1));
+    if (res == NULL)
+        return (NULL);
+    res[length_result] = '\0';
+    while (i1 >= 0 && i2 >= 0) {
+        add_2digits(&(res[i_res - 1]), &(res[i_res]), &(nbr1[i1]), &(nbr2[i2]));
+        decrement_indexes(&i1, &i2, &i_res);
+    }
+    if (i1 < 0 && i2 >= 0) {
+        add_1digit(&(res[i_res - 1]), &(res[i_res]), &(nbr2[i2]));
+        decrement_indexes(&i1, &i2, &i_res);
+    } else if (i2 < 0 && i1 >= 0) {
+        add_1digit(&(res[i_res - 1]), &(res[i_res]), &(nbr1[i1]));
+        decrement_indexes(&i1, &i2, &i_res);
+    }
+    digits_to_ascii(res, length_result);
+    return (res);
+}
+
+/*char *sub_strings(char *nbr1, char *nbr2, int length_nbr1, int length_nbr2)
+{
+    //!
+    return (result);
+}*/
+
+char *get_result(char *nbr1, char *nbr2, int length_nbr1, int length_nbr2)
+{
+    char *result = NULL;
+
+    if (get_sign(nbr1) == '+' && get_sign(nbr2) == '+') {
+        result = add_strings(nbr1, nbr2, length_nbr1, length_nbr2);
+        result[0] = '+';
+    } else if (get_sign(nbr1) == '-' && get_sign(nbr2) == '-') {
+        result = add_strings(nbr1, nbr2, length_nbr1, length_nbr2);
+        result[0] = '-';
+    } /*else {
+        result = sub_strings(nbr1, nbr2, length_nbr1, length_nbr2);
+        //! mettre le bon signe
+    }*/
+    return (result);
+}
+
+void display_result(char *result)
 {
     int i = 0;
 
-    digits_to_ascii(result, length_result);
     if (result[i] == '-')
         write(1, &(result[i]), 1);
     for (i = 0; result[i] <= '0'; i++);
@@ -34,18 +98,19 @@ void display_result(char *result, int length_result)
 int main(int argc, char **argv)
 {
     char *result = NULL;
-    int length_result = 0;
+    int length_nbr1 = 0;
+    int length_nbr2 = 0;
 
     if (detect_input_errors(argc, argv) == EXIT_FAILURE)
         return (EXIT_FAILURE);
+    length_nbr1 = my_strlen(argv[1]);
+    length_nbr2 = my_strlen(argv[2]);
     ascii_to_digits(argv[1]);
     ascii_to_digits(argv[2]);
-    length_result = max(my_strlen(argv[1]), my_strlen(argv[2])) + 1;
-    result = malloc(sizeof(char) * (length_result + 1));
+    result = get_result(argv[1], argv[2], length_nbr1, length_nbr2);
     if (result == NULL)
         return (EXIT_FAILURE);
-    add_strings(argv[1], argv[2], result);
-    display_result(result, length_result);
+    display_result(result);
     free(result);
     return (EXIT_SUCCESS);
 }
